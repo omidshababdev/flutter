2510a96897b697dc4edb7e8c03eb92a639ad34da
Omid Shabab <100057185+omidshababdev@users.noreply.github.com>
2023-01-29 09:24:53 +0330

    first commit

---
+++ awesome_notifications_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AwesomeNotificationsApp());
+}
+
+class AwesomeNotificationsApp extends StatefulWidget {
+  const AwesomeNotificationsApp({super.key});
+
+  @override
+  State<AwesomeNotificationsApp> createState() =>
+      _AwesomeNotificationsAppState();
+}
+
+class _AwesomeNotificationsAppState extends State<AwesomeNotificationsApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
